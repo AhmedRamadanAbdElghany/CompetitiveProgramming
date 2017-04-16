c58fcb2fa51731b4eea01fac74a50f441dc30026
AhmedRamadanAbdElghany <ahmeditaly1537@gmail.com>
2017-04-16 15:07:39 +0200

    Create CF190-D2-D.cpp

---
+++ CodeForces/CF190-D2-D.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#define all(v)          v.begin(),v.end()
+#define allr(v)         v.rbegin(),v.rend()
+#define rep(i,m)        for(int i=0;i<m;i++)
+#define REP(i,k,m)      for(int i=k;i<m;i++)
+#define repd(i,m)        for(int i=m;i>=0;i--)
+#define P(x)                cout<<#x<<" = { "<<x<<" }\n"
+#define mem(a,b)        memset(a,b,sizeof(a))
+#define mp              make_pair
+#define pb              push_back
+#define OO ((ll)1e7)
+#define ooo -100000000
+#define small  INT_MIN;
+#define big  INT_MAX;
+using namespace std;
+typedef long long ll;
+typedef double db;
+typedef long double       ld;
+typedef vector<int>       vi;
+typedef vector<bool>       vb;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef bitset<20> MASK;
+#define INF 10000
+#define sz(v)          ((int)((v).size()))
+//std::ios_base::sync_with_stdio(false); means  i will not deal with c lang that will speed
+const double EPS = 1e-9;
+typedef complex<double> point;
+
+#define X real()
+#define Y imag()
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+const int oo = (int) 1e9;
+const double PI = 2 * acos(0.0);
+const double eps = 1e-7;
+#define pi 1000000007
+#define black 0;
+#define white 1;
+const int MAXN=1e5+10;
+int dx[] = {1, 0, 0, -1, -1, -1, 1, 1};
+int dy[] = {0, 1, -1, 0, 1, -1, 1, -1};
+/*
+ given an array of integers a and number k find the number of subarrays of the array of numbers a, which has at least k equal numbers.
+ we use two pointer to detrmine first occurence of valid sub array then all the next number can be in new subarrays .
+ then move the first pointer and while it's value is not equal last value of the first subarray move it again and count as element after 
+ the last pointer can be construct new subarray .
+*/
+map < int, int > M ;
+int main()
+{
+
+    int n, k, a[400009] ;
+    ll ans = 0 ;
+    cin >> n >> k ;
+    rep( i, n )
+    cin >> a[i];
+    int s = 0, e = 0 ;
+
+    while(e < n )
+    {
+        M[a[e]]++;
+      
+      if (M[a[e]] == k )
+        {
+            while(a[s] != a[e] && e >= s ) 
+            {
+                ans += n - e ;
+                M[a[s]]--;
+                s++ ;
+            }
+            if (e >= s )
+                M[a[s]]--,s++,ans+=n-e;
+        }
+        e++;
+    }
+    cout << ans << endl;
+}
+
+
+
