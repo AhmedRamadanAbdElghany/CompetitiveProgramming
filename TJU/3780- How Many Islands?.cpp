c468f2d128ed97c0b1ee90d0d926ab3930734164
AhmedRamadanAbdElghany <ahmeditaly1537@gmail.com>
2017-04-24 20:46:01 +0200

    Create 3780- How Many Islands?.cpp

---
+++ TJU/3780- How Many Islands?.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#define all(v)          v.begin(),v.end()
+#define allr(v)         v.rbegin(),v.rend()
+#define rep(i,m)        for(int i=0;i<m;i++)
+#define REP(i,k,m)      for(int i=k;i<m;i++)
+#define repd(i,m)        for(int i=m;i>=0;i--)
+#define P(x)                cout<<#x<<" = { "<<x<<" }\n"
+#define mem(a,b)        memset(a,b,sizeof(a))
+#define mp              make_pair
+#define pb              push_back
+#define OO ((ll)1e7)
+#define ooo -100000000
+#define small  INT_MIN;
+#define big  INT_MAX;
+using namespace std;
+typedef long long ll;
+typedef double db;
+typedef long double       ld;
+typedef vector<int>       vi;
+typedef vector<bool>       vb;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef bitset<20> MASK;
+#define INF 10000
+#define sz(v)          ((int)((v).size()))
+//std::ios_base::sync_with_stdio(false); means  i will not deal with c lang that will speed
+const double EPS = 1e-9;
+typedef complex<double> point;
+
+#define X real()
+#define Y imag()
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+const int oo = (int) 1e9;
+const double PI = 2 * acos(0.0);
+const double eps = 1e-7;
+#define pi 1000000007
+#define black 0;
+#define white 1;
+const int MAXN=1e5+10;
+int dx[] = {1, 0, 0, -1, -1, -1, 1, 1};
+int dy[] = {0, 1, -1, 0, 1, -1, 1, -1};
+int n, m ;
+bool vis[100][100];
+bool aa[100][100];
+
+/*
+need to find number of CC so dfs from every unvisited cell and find all adjcent .
+*/
+
+bool valid (int  a, int b )
+{
+    return a >= 0 && a < n && b >= 0 && b < m && aa[a][b] == 1 && vis[a][b] == 0 ;
+}
+void dfs(int a, int b )
+{
+    vis[a][b] = 1 ;
+    rep( i, 8 )
+    {
+        int na = dx[i] + a ;
+        int nb = dy[i] + b ;
+        if (valid (na, nb ))
+            dfs(na, nb );
+    }
+
+}
+
+int main()
+{
+
+    while(cin >> m >> n )
+    {
+        if ( n == 0 && m == 0 ) break ;
+        mem (vis, 0 );
+        rep( i, n )
+        rep( j, m )
+        cin >> aa[i][j];
+        int  cnt = 0 ;
+        rep( i, n )
+        rep( j, m )
+        if (aa[i][j] == 1 && vis[i][j] == 0 )
+            cnt++, dfs(i, j );
+
+        cout << cnt << endl;
+    }
+}
