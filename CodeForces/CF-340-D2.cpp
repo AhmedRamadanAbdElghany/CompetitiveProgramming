1f4104bbbc3590eac8565784d47be43c9dce973f
AhmedRamadanAbdElghany <ahmeditaly1537@gmail.com>
2017-07-22 19:18:26 +0200

    Create CF-340-D2.cpp

---
+++ CodeForces/CF-340-D2.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define all(v)          v.begin(),v.end()
+#define allr(v)         v.rbegin(),v.rend()
+#define rep(i,m)        for(int i=0;i<m;i++)
+#define REP(i,k,m)      for(int i=k;i<m;i++)
+#define repd(i,m)        for(int i=m;i>=0;i--)
+#define P(x)                cout<<#x<<" = { "<<x<<" }\n"
+#define mem(a,b)        memset(a,b,sizeof(a))
+#define mp              make_pair
+#define pb              push_back
+#define OO ((ll)1e12)
+#define ooo -100000000
+#define small  INT_MIN;
+#define big  INT_MAX;
+using namespace std;
+typedef long long ll;
+typedef double db;
+typedef long double       ld;
+typedef vector<int>       vi;
+typedef vector<bool>       vb;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef bitset<20> MASK;
+#define INF 10000
+#define sz(v)          ((int)((v).size()))
+const int oo = (int) 1e9;
+const double PI = 2 * acos(0.0);
+const double eps = 1e-7;
+/*
+tracing more example find that every maximum independent is LIS .
+*/
+
+int CeilIndex(std::vector<int> &v, int l, int r, int key) {
+    while (r-l > 1) {
+    int m = l + (r-l)/2;
+    if (v[m] >= key)
+        r = m;
+    else
+        l = m;
+    }
+
+    return r;
+}
+
+int LongestIncreasingSubsequenceLength(std::vector<int> &v) {
+    if (v.size() == 0)
+        return 0;
+
+    std::vector<int> tail(v.size() , 0);
+    int length = 1; // always points empty slot in tail
+
+    tail[0] = v[0];
+   REP( i, 1 , sz(v)){
+            if (v[i] < tail[0])
+            tail[0] = v[i];
+        else if (v[i] > tail[length-1])
+            tail[length++] = v[i];
+        else
+            tail[CeilIndex(tail, -1, length-1, v[i])] = v[i];
+    }
+    return length;
+}
+
+
+int main()
+{
+int n ;
+    cin >> n ;
+vi v (n);
+    rep( i , n )
+    cin >> v[i];
+    cout << LongestIncreasingSubsequenceLength(v) << endl;
+
+    return 0;
+}
