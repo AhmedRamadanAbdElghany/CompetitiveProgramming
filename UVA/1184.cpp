b45e02ab2d1a09869a97c3f33d6824235760fa6f
AhmedRamadanAbdElghany <ahmeditaly1537@gmail.com>
2017-08-06 02:08:12 +0200

    Create 1184.cpp

---
+++ UVA/1184.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+#define all(v)          v.begin(),v.end()
+#define allr(v)         v.rbegin(),v.rend()
+#define rep(i,m)        for(int i=0;i<m;i++)
+#define REP(i,k,m)      for(int i=k;i<m;i++)
+#define repd(i,m)        for(int i=m;i>=0;i--)
+#define P(x)                cout<<#x<<" = { "<<x<<" }\n"
+#define mem(a,b)        memset(a,b,sizeof(a))
+#define mp              make_pair
+#define pb              push_back
+#define OO ((ll)1e12)
+#define ooo -100000000
+#define small  INT_MIN;
+#define big  INT_MAX;
+using namespace std;
+typedef long long ll;
+typedef double db;
+typedef long double       ld;
+typedef vector<int>       vi;
+typedef vector<bool>       vb;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef bitset<20> MASK;
+#define INF 10000
+#define sz(v)          ((int)((v).size()))
+//std::ios_base::sync_with_stdio(false); means  i will not deal with c lang that will speed
+const int oo = (int) 1e9;
+const double PI = 2 * acos(0.0);
+const double eps = 1e-7;
+#define pi 1000000007
+#define black 0;
+#define white 1;
+const int MAXN=1e5+10;
+
+int dx[] = {1, 0, 0, -1, -1, -1, 1, 1};
+int dy[] = {0, 1, -1, 0, 1, -1, 1, -1};
+
+/*
+problem decription :
+given n points and one paratrooper can lands at a point and visit all nodes in it's path .
+we need the minimum number of paratrooper that can visit all nodes .
+
+solution : 
+every node can visit all nodes in it's path so we need to visit every node once .
+so all nodes need to be covered in one paratrooper 
+so we need minimum paratrooper that cover all nodes it is equal to min path coverage problem = N - maximum matching .
+
+*/
+
+
+int  N;
+vector< vector < int >  > adj_list;
+int  prevv[200];
+bool mark[200];
+bool find_match(int node)
+{
+    if (node == -1) return true;
+    for (int i = 0; i < sz(adj_list[node]); i++)
+    {
+        int match = adj_list[node][i];
+        if (mark[match]) continue;
+        mark[match] = true;
+        if (find_match(prevv[match]))
+        {
+            prevv[match] = node;
+            return true;
+        }
+    }
+    return false;
+}
+
+int max_matching()
+{
+    memset(prevv, -1, sizeof(prevv));
+    int matching = 0;
+    for (int i = 0; i < N; i++)
+    {
+        memset(mark, 0, sizeof(mark));
+        if (find_match(i)) matching++;
+    }
+    return matching;
+}
+
+
+
+int main()
+{
+
+//#ifndef Ahmed_Ramadan
+///  freopen("a.txt", "rt", stdin);
+// freopen("a.txt", "wt", stdout);
+//#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int m, a, b,tc ;
+    cin >> tc ;
+    while(tc--)
+    {
+        adj_list.clear();
+        cin >> N >> m ;
+        adj_list.resize(N);
+        rep( i, m )
+        {
+            cin >> a>> b ;
+            adj_list[a].pb(b);
+        }
+        cout << N - max_matching() << endl;
+    }
+}
+
